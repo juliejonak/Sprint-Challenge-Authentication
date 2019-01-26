8c7b8c33e4702f7759019941288ce7947056f920
Julie Jonak <juliejonak10@gmail.com>
2019-01-26 13:20:50 -0600

    Jokes endpoint has loading status and conditional rendering based on logged in status

--- jokes/src/components/jokes.js
+++ jokes/src/components/jokes.js
@@ -7,15 +7,65 @@ class Jokes extends React.Component {
         super(props);
         this.state = {
             jokes: [],
-            deniedAccess: true
+            loading: true,
+            loggedIn: false
         }
     }
 
     componentDidMount = () => {
+        const token = localStorage.getItem('jwt');
+        const options = {
+            headers: {
+                Authorization: token,
+            }
+        }
 
+        axios.get('http://localhost:3300/api/jokes', options)
+            .then(response => {
+                this.setState({
+                    jokes: response.data,
+                    loggedIn: true,
+                    loading: false
+                })
+            })
+            .catch(err => {
+                this.setState({
+                    loggedIn: false,
+                    loading: false
+                })
+            })
     }
 
     content = () => {
+        if(this.state.loading){
+            return(
+                <div>
+                    <h3>Loading...</h3>
+                </div>
+            )
+        }
+        
+        if(this.state.loggedIn){
+            return(
+                <div>
+                    <h1 className='header'>EPIC DAD JOKES</h1>
+                    <ul>
+                        {this.state.jokes.map(joke => {
+                            return(
+                            <div key={joke.id} className="joke">
+                                <p>{joke.joke}</p>
+                            </div> )
+                        })}
+                    </ul>
+                </div>
+            )
+        } else {
+            return(
+                <div>
+                    <p>You must be logged in to view the epic Dad Jokes!</p>
+                </div>
+            )
+        }
 
     }
 
