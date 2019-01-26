48acdd09b616caa1ae2e57e4246629a36c472b43
Julie Jonak <juliejonak10@gmail.com>
2019-01-26 13:10:03 -0600

    Added register and login forms that function

--- jokes/src/components/register.js
+++ jokes/src/components/register.js
@@ -12,18 +12,61 @@ class Register extends React.Component {
         }
     }
 
-    inputHandler = () => {
-
+    inputHandler = (e) => {
+        this.setState({ [e.target.name]: e.target.value })
     }
 
-    register = () => {
+    register = (e) => {
+        e.preventDefault();
+        const endpoint = 'http://localhost:3300/api/register';
+        const newUser = {
+            username: this.state.username,
+            password: this.state.password
+        };
 
+        axios.post(endpoint, newUser)
+            .then(res => {
+                this.props.history.push('/login')
+            })
+            .catch(err => {
+                this.setState({
+                    error: true
+                })
+            })
     }
 
     render(){
         return(
             <div>
-                
+                <h1>Register for Dad Jokes!</h1>
+
+                <form onSubmit={this.register}>
+                    <div>
+                        <input
+                            onChange={this.inputHandler}
+                            type="text"
+                            placeholder="Username"
+                            value={this.state.username}
+                            name="username"
+                        >
+                        </input>
+                    </div>
+
+                    <div>
+                        <input
+                            onChange={this.inputHandler}
+                            type="password"
+                            placeholder="Password"
+                            value={this.state.password}
+                            name="password"
+                        >
+                        </input>
+                    </div>
+
+                    <div className="button">
+                        <button type="submit">Submit</button>
+                    </div>
+                </form>
             </div>
         )
     }
